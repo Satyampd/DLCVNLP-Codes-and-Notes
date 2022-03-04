b9b35d7b7b9146811c72756ad9eaf299bff0b4da
Satyam Prasad Tiwari <satyamprasadtiwari@gmail.com>
2022-03-04 12:45:54 +0530

    adding tensorboard and callback code

--- Tensorboard and Callbacks - 10th Oct/src/training.py
+++ Tensorboard and Callbacks - 10th Oct/src/training.py
@@ -1,8 +1,9 @@
-from utils.common import read_config
+from gc import callbacks
+from utils.common import read_config, get_log_path, get_unique_file_name
 from utils.data_mgmt import get_data
 from utils.model import create_model, save_model, save_plot
 import os
-
+from tensorflow import keras
 
 def training(config_path):
     config = read_config(config_path)
@@ -16,9 +17,32 @@ def training(config_path):
     no_of_classes = config["params"]["no_of_classes"]
     model = create_model(LOSS_FUNCTION=loss_function,OPTIMIZER=optimizer, METRICS=metrics, no_of_classes=no_of_classes)
 
+    # creating tb log path
+    unique_log_dir_name = get_log_path()
+    log_dir = config["logs"]["tensorboard_logs_dir"]
+    log_dir_path = os.path.join(log_dir,unique_log_dir_name)
+    os.makedirs(log_dir_path, exist_ok=True)
+
+    # tensorboard callback
+    tensorboard_cb = keras.callbacks.TensorBoard(log_dir = log_dir_path)
+    # Early stopping 
+    early_stop_cb = keras.callbacks.EarlyStopping(patience=5, restore_best_weights= True)
+    # Checkpoint callbacks
+    ckpt_file_name = 'model.{epoch:02d}-{val_loss:.2f}.h5'
+
+    ckpt_model_dir = config["artifacts"]["ckpt_model_dir"] 
+    artifacts_dir = config["artifacts"]["artifacts_dir"]
+    ckpt_path = os.path.join(artifacts_dir,ckpt_model_dir)
+    os.makedirs(ckpt_path, exist_ok= True)
+    ckpt_path_dir = os.path.join(ckpt_path,ckpt_file_name)
+    checkpoint_cb = keras.callbacks.ModelCheckpoint( ckpt_path_dir, save_best_only= True)
+    # list of callbacks 
+    callback_list = [tensorboard_cb, early_stop_cb, checkpoint_cb]
+
+
     epochs = config["params"]["epochs"]
     batch_size = config["params"]["epochs"]
-    model.fit(X_train, y_train, epochs = 2, validation_data= (X_valid, y_valid), batch_size = batch_size)
+    model.fit(X_train, y_train, epochs = 2, validation_data= (X_valid, y_valid), batch_size = batch_size, callbacks = callback_list)
 
     artifacts_dir = config["artifacts"]["artifacts_dir"]
     model_dir = config["artifacts"]["model_dir"]
@@ -32,6 +56,8 @@ def training(config_path):
     plots_dir_path = os.path.join(artifacts_dir,plot_dir)
     os.makedirs(plots_dir_path, exist_ok=True)
     save_plot(data=model.history.history, plots_dir= plots_dir_path )
+
+
 
 if __name__ == '__main__':
 
--- Tensorboard and Callbacks - 10th Oct/src/utils/common.py
+++ Tensorboard and Callbacks - 10th Oct/src/utils/common.py
@@ -1,6 +1,18 @@
 import yaml
+import time
+import os
 
 def read_config(config_path):
     with open(config_path) as config_file:
         content = yaml.safe_load(config_file)
     return content
+
+def get_log_path(log_dir = "logs/fit"):
+  uniqueName = time.strftime("log_%Y_%m_%d_%H_%M_%S")
+  log_path = os.path.join(log_dir,uniqueName )
+
+  return log_path
+
+def get_unique_file_name(filename):
+    unique_filename = time.strftime(f"%Y%m%d_%H%M%S_{filename}")
+    return unique_filename
